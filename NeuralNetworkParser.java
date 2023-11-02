5175c4721a03e7b0d93b197e5d76c20d17df1dd8
Daniel Runningen <daniel.runningen+github@outlook.com>
2023-11-02 14:48:57 -0500

    Non-breaking changes

--- NeuralNetworkParser.java
+++ NeuralNetworkParser.java
@@ -1,7 +1,6 @@
 import java.util.Arrays;
 
 public class NeuralNetworkParser {
-
     public static void main(String[] args) {
         String weightsLine = "[[-4.0, -5.0, 1.0, -2.0], [-5.0, 3.0, 0.0, -9.0], [-4.0, 9.0, -4.0, 5.0], [3.0, 5.0, 9.0, -2.0], [-8.0, -6.0, 1.0, 6.0]]";
         String biasesLine = "[[-3.0], [-7.0], [6.0], [-9.0], [9.0]]";
@@ -15,22 +14,19 @@ public class NeuralNetworkParser {
 
     private static double[][] parseWeights(String weightsLine) {
         String[] rows = weightsLine.replaceAll("\\[\\[", "")
-                                   .replaceAll("\\]\\]", "")
-                                   .split("\\], \\[");
+                .replaceAll("\\]\\]", "").split("\\], \\[");
         double[][] weights = new double[rows.length][];
         for (int i = 0; i < rows.length; i++) {
             weights[i] = Arrays.stream(rows[i].split(", "))
-                               .mapToDouble(Double::parseDouble)
-                               .toArray();
+                    .mapToDouble(Double::parseDouble).toArray();
         }
         return weights;
     }
 
     private static double[] parseBiases(String biasesLine) {
-        return Arrays.stream(biasesLine.replaceAll("\\[\\[", "")
-                                       .replaceAll("\\]\\]", "")
-                                       .split("\\], \\["))
-                     .mapToDouble(Double::parseDouble)
-                     .toArray();
+        return Arrays
+                .stream(biasesLine.replaceAll("\\[\\[", "")
+                        .replaceAll("\\]\\]", "").split("\\], \\["))
+                .mapToDouble(Double::parseDouble).toArray();
     }
 }
