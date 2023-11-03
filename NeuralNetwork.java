862b4bdaea8db913b7618f7a20fe103b8e6f9999
Daniel Runningen <daniel.runningen+github@outlook.com>
2023-11-03 14:54:47 -0500

    Added CEM optimization

--- NeuralNetwork.java
+++ NeuralNetwork.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.Random;
 
 public class NeuralNetwork {
@@ -27,6 +28,78 @@ public class NeuralNetwork {
    // To Compare with Example Output from Our Network (I don't expect it to match but incase it does)
    public boolean compareOutputWithExample(double[] computedOutput) {
       return Arrays.equals(computedOutput, this.exampleOutput);
+   }
+
+   // Referencing from the wiki article:
+   // https://en.wikipedia.org/wiki/Cross-entropy_method
+   public double[] cem() {
+      Random r = new Random();
+
+      // Simulation Parameters
+      int maxIters = 10000;
+      int iter = 0;
+      double epsilon = 0.00001; // How close should our variance get?
+      int sampleSize = 100;
+      int keep = 10; // Of the best samples
+
+      // Data Structures
+      double[] bestMeans = new double[layers.get(0).getNodeCount()];
+      Arrays.fill(bestMeans, 0.0);
+      double[] variance = new double[bestMeans.length];
+      Arrays.fill(variance, keep);
+      double[][] samples = new double[sampleSize][bestMeans.length];
+      ArrayList<Double> magnitudes;
+
+      // Variables for intermediary math
+      double[] temp;
+      int minIndex;
+      double meanSum, varSum;
+      double varTotal = Double.MAX_VALUE;
+      double oldVarTotal = Double.MIN_VALUE;
+
+      // While we aren't bored and there are still changes in variance...
+      while (iter < maxIters && Math.abs(varTotal - oldVarTotal) > epsilon) {
+         magnitudes = new ArrayList<Double>();
+         for (int i = 0; i < samples.length; i++) {
+            // Generate random inputs from a gaussian distribution.
+            for (int j = 0; j < bestMeans.length; j++) {
+               samples[i][j] = r.nextGaussian() * Math.sqrt(variance[j])
+                     + bestMeans[j];
+            }
+            // Calculate how far away we are from the goal of an all zero output.
+            magnitudes.add(Arrays.stream(this.computeOutput(samples[i]))
+                  .map(Math::abs).sum()); // Sum of absolute values
+         }
+         // Do some sorting for the top performers.
+         for (int i = 0; i < keep; i++) {
+            minIndex = magnitudes.indexOf(Collections.min(magnitudes));
+            temp = samples[minIndex];
+            samples[minIndex] = samples[i];
+            samples[i] = temp;
+            magnitudes.set(minIndex, magnitudes.get(i));
+            magnitudes.set(i, Double.MAX_VALUE);
+         }
+         // Keep track of the total change.
+         oldVarTotal = varTotal;
+         varTotal = 0;
+         // Find the means and variance of the best results.
+         for (int i = 0; i < bestMeans.length; i++) {
+            meanSum = 0;
+            varSum = 0;
+            for (int j = 0; j < keep; j++) {
+               meanSum += samples[j][i];
+            }
+            bestMeans[i] = meanSum / keep;
+            for (int j = 0; j < keep; j++) {
+               varSum += Math.pow(samples[j][i] - bestMeans[i], 2);
+            }
+            variance[i] = varSum / keep;
+            varTotal += variance[i];
+         }
+         iter++;
+      }
+      System.out.println("Iterations: " + iter);
+      return bestMeans;
    }
 
    // Optimization algorithm to minimize the output magnitude
--- NeuralNetworkOptimization.java
+++ NeuralNetworkOptimization.java
@@ -1,6 +1,5 @@
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class NeuralNetworkOptimization {
     public static void main(String[] args) {
@@ -22,20 +21,45 @@ public class NeuralNetworkOptimization {
         }
 
         for (NeuralNetwork network : networks) {
-            //double[] optimalInput = network.findOptimalInput();
-            // System.out.println("Optimal input for the network: " +
-            // Arrays.toString(optimalInput));
-            double[] networkOutput = network
-                    .computeOutput(network.getExampleInput());
-            System.out.println("Network output for the given example input: "
-                    + Arrays.toString(networkOutput));
-            System.out.println("Example output from file: "
-                    + Arrays.toString(network.getExampleOutput()));
+            //// Part 1
+            // double[] networkOutput = network
+            //         .computeOutput(network.getExampleInput());
+            // System.out.println("Network output for the given example input: "
+            //         + Arrays.toString(networkOutput));
+            // System.out.println("Example output from file: "
+            //         + Arrays.toString(network.getExampleOutput()));
 
             // Uncomment the following lines to check if the computed output matches the example output
             // boolean match = network.compareOutputWithExample(networkOutput);
             // 
             // System.out.println("Does the computed output match the example output? " + match);
+
+            double[] optimalInput = network.findOptimalInput();
+            System.out.println("Optimal input from Random Search: [");
+            for (double val : optimalInput) {
+                System.out.println("\t" + val);
+            }
+            System.out.println("]");
+
+            double[] optimum = network.cem();
+            System.out.println("Optimal input from Gaussian: [");
+            for (double val : optimum) {
+                System.out.println("\t" + val);
+            }
+            System.out.println("]");
+
+            System.out.println("Vector length from Random Search: "
+                    + vectorLength(network.computeOutput(optimalInput)));
+            System.out.println("Vector length from Gaussian: "
+                    + vectorLength(network.computeOutput(optimum)));
         }
+    }
+
+    public static double vectorLength(double[] vec) {
+        double sum = 0;
+        for (double x : vec) {
+            sum += x * x;
+        }
+        return Math.sqrt(sum);
     }
 }
